0d225bf8976140eeb28a6e107a1a6beb21f68546
harshit duggal <harshitduggal56@gmail.com>
2026-03-30 14:31:07 +0530

    feat: add sapphire formula

---
+++ Formula/sapphire.rb
@@ -0,0 +1,23 @@
+class Sapphire < Formula
+  desc "Terminal-first AI assistant for software development"
+  homepage "https://github.com/duggal1/Sapphire-cli"
+  url "https://github.com/duggal1/Sapphire-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "ca0661beb640a3a0cc982a7bb92b3b7405e713890f2325460b8dc383a6f3f4aa"
+  license "FSL-1.1-MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/duggal1/Sapphire-cli/internal/version.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "."
+
+    generate_completions_from_executable(bin/"sapphire", "completion")
+
+    manpage = Utils.safe_popen_read(bin/"sapphire", "man")
+    (man1/"sapphire.1").write(manpage)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/sapphire --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-sapphire-cli
+
+Homebrew tap for Sapphire CLI.
+
+Install with:
+
+```bash
+brew install duggal1/sapphire-cli/sapphire
+```
